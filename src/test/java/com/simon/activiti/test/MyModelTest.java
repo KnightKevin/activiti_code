267623005d334b6ca4e08171003f8ed667ff68a4
Simon <ketteringkevin@sina.com>
2020-07-21 16:13:42 +0800

    解决了动态部署问题

--- src/test/java/com/simon/activiti/test/MyModelTest.java
+++ src/test/java/com/simon/activiti/test/MyModelTest.java
@@ -1,5 +1,7 @@
 package com.simon.activiti.test;
 
+import com.alibaba.fastjson.JSON;
+import lombok.Data;
 import org.activiti.bpmn.converter.BpmnXMLConverter;
 import org.activiti.bpmn.model.*;
 import org.activiti.bpmn.model.Process;
@@ -11,7 +13,10 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 利用BpmnModel来部署
@@ -22,19 +27,76 @@ public class MyModelTest extends ApplicationTest {
 
     private DeploymentBuilder builder;
 
+    private final  static String key = "MyRequest";
+
     @Before
     public void hhh() {
         builder = repositoryService.createDeployment();
     }
 
-
+    /**
+     * 通过BpmnModel来部署流程定义，切记resourceName参数一定要加bpmn20.xml或者bpmn后缀
+     * 不然就会部署个寂寞
+     * */
     @Test
-    public void deployProcess() {
-        builder.addBpmnModel("MyRequest", createProcessModel())
+    public void deployBpmnModelProcess() {
+        builder.addBpmnModel("MyRequest.bpmn20.xml", createProcessModel())
                 .name("Manual")
+                .key("manual")
                 .enableDuplicateFiltering()
                 .deploy()
         ;
+    }
+
+    /**
+     * 自己解析出我需要的流程节点后构建出一个我自己设计的数据结构可以作为接口请求参数也可以作为给前端绘图用的数据结构
+     * */
+    @Test
+    public void getProcessToJson() {
+        // 准确来说应该是获取实例所关联的流程定义
+        List<ProcessInstance> instances = runtimeService.createProcessInstanceQuery().processDefinitionKey(key).list();
+        ProcessDefinition processDefinition = null;
+        BpmnModel bpmnModel = null;
+
+        List<UserTaskParam> userTaskList = new LinkedList<>();
+        List<SequenceFlowParam> sequenceFlowList = new LinkedList<>();
+        FlowBpmnParam flowBpmnParam = new FlowBpmnParam();
+
+        for (ProcessInstance instance : instances) {
+            log.info("definitionKey       = "+instance.getProcessDefinitionKey());
+            log.info("version             = "+instance.getProcessDefinitionVersion());
+            log.info("processDefinitionId = "+instance.getProcessDefinitionId());
+            log.info("deploymentId = "+instance.getDeploymentId());
+
+            bpmnModel = repositoryService.getBpmnModel(instance.getProcessDefinitionId());
+            Process process = bpmnModel.getProcessById(key);
+
+            Collection<FlowElement> list = process.getFlowElements();
+            log.info("the size of map is "+list.size());
+
+            userTaskList.clear();
+            sequenceFlowList.clear();
+
+            for (FlowElement element : list) {
+                if (element instanceof UserTask) {
+                    userTaskList.add(new UserTaskParam(element.getId(), ((UserTask) element).getAssignee()));
+                }
+
+                if (element instanceof SequenceFlow) {
+                    sequenceFlowList.add(new SequenceFlowParam(((SequenceFlow) element).getSourceRef(), ((SequenceFlow) element).getTargetRef()));
+                }
+            }
+
+            flowBpmnParam.setUserTaskList(userTaskList);
+            flowBpmnParam.setSequenceFlowList(sequenceFlowList);
+
+            log.info(JSON.toJSONString(flowBpmnParam));
+            log.info("######################################################");
+        }
+
+
+
+
     }
 
     @Test
@@ -55,23 +117,16 @@ public class MyModelTest extends ApplicationTest {
 
     @Test
     public void startProcess() {
-        ProcessInstance instance = runtimeService.startProcessInstanceByKey("MyRequest");
+        ProcessInstance instance = runtimeService.startProcessInstanceByKey(key);
         log.info("process instance id is "+instance.getId());
-    }
-
-    @Test
-    public void createXml() {
-        BpmnXMLConverter bpmnXMLConverter = new BpmnXMLConverter();
-        byte[] xml = bpmnXMLConverter.convertToXML(createProcessModel());
-        log.info(new String(xml));
     }
 
     public BpmnModel createProcessModel() {
         BpmnModel model = new BpmnModel();
 
         Process process = new Process();
-        process.setId("MyRequest");
-        process.setName("自己的模型对象");
+        process.setId(key);
+        process.setName("自己的模型对象-hahaha");
 
         model.addProcess(process);
 
@@ -84,11 +139,13 @@ public class MyModelTest extends ApplicationTest {
         UserTask userTask = new UserTask();
         userTask.setName("处理请求");
         userTask.setId("handleRequest");
+        userTask.setAssignee("Simon");
         process.addFlowElement(userTask);
 
         // 结束事件
         EndEvent endEvent = new EndEvent();
         endEvent.setId("endEvent");
+        endEvent.setName("endEvent");
         process.addFlowElement(endEvent);
 
         // 添加流程顺序
@@ -98,4 +155,33 @@ public class MyModelTest extends ApplicationTest {
         return model;
     }
 
+}
+
+// 定义几个实体
+@Data
+class UserTaskParam {
+    private String id;
+    private String assignee ="";
+    public UserTaskParam(String id, String assignee) {
+        this.id = id;
+        this.assignee = assignee;
+    }
+}
+
+@Data
+class SequenceFlowParam {
+    private String source;
+    private String target;
+    public SequenceFlowParam(String source, String target) {
+        this.source = source;
+        this.target = target;
+    }
+}
+
+@Data
+class FlowBpmnParam {
+    private String startEvent = "startEvent";
+    private String endEvent   = "endEvent";
+    private List<UserTaskParam> userTaskList;
+    private List<SequenceFlowParam> sequenceFlowList;
 }
